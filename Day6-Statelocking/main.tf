7d6b48c8264e933cb1a81df0c22685fb87f23244
Vishwakarma <pradeep.vishwakarma@capgemini.com>
2026-08-12 15:20:45 +0530

    Adding main.tf file

--- Day6-Statelocking/main.tf
+++ Day6-Statelocking/main.tf
@@ -4,3 +4,11 @@ resource "aws_vpc" "name" {
     Name = "dev"
   }
 }
+
+resource "aws_subnet" "name" {
+  vpc_id = aws_vpc.name.id
+  cidr_block = "10.0.0.0/24"
+  tags = {
+    Name = "prod-subnet"
+  }
+}
